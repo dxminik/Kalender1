f9a760718c5315ae8650cfe65cb42c2cba0aa778
Dominik <Dominikbecht@icloud.com>
2025-02-23 15:03:39 +0100

    Frontend
    Anfang Frontent

--- main.py
+++
@@ -1,6 +0,0 @@
-#create_user("Aubergine", "Beutel", "Weiblich", "23.01.1999", "Tiere, Musik, Pflanzen")
-from backend.database.manage_users import get_user_by_id
-
-print(get_user_by_id(1))
-ausgabe = get_user_by_id(2)[5]
-print(ausgabe)

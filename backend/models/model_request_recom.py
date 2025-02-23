762acd6554232db09c98ac3551c16a6b27b94f1e
Dominik <Dominikbecht@icloud.com>
2025-02-23 21:44:31 +0100

    Ohne API Key

--- backend/models/model_request_recom.py
+++ backend/models/model_request_recom.py
@@ -1,7 +1,7 @@
 from openai import OpenAI
 
 
-client = OpenAI(api_key='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')
+
 
 def gib_empfehlung(daten):
     prompt = f"Basierend auf den folgenden Nutzerdaten, gib eine Empfehlung für den Benutzer: {daten}"

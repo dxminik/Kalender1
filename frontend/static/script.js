0fc869144b8d45acb4efbff0e6d2ae6ddc9d279b
SamysSabber <118390382+SamysSabber@users.noreply.github.com>
2025-02-23 15:45:21 +0100

    Submit Button und Description styling

--- frontend/static/script.js
+++ frontend/static/script.js
@@ -5,14 +5,6 @@ darkModeButton.addEventListener('click', (e) => {
     console.log('Dark Mode Button Clicked');
 })
 
-//HeaderSearch
-const headerSearch = document.getElementById('headerSearch');
-const searchButton = document.getElementById('searchButton');
-
-headerSearch.addEventListener('input', (e) => {
-    console.log(e.target.value);
-})
-
 //FeelingInput
 const feelingInput = document.getElementById('feelingInput');
 const feelingOutput = document.getElementById('feelingOutput');

c9a4605d102b6c5ae3c79aef148d817d598e94b6
SamysSabber <118390382+SamysSabber@users.noreply.github.com>
2025-02-23 15:05:50 +0100

    Style und Feeling-Regler geändert

--- frontend/static/index.js
+++ frontend/static/index.js
@@ -13,4 +13,15 @@ headerSearch.addEventListener('input', (e) => {
     console.log(e.target.value);
 })
 
+//FeelingInput
+const feelingInput = document.getElementById('feelingInput');
+const feelingOutput = document.getElementById('feelingOutput');
+let feelingValue = feelingInput.value;
+
+
+feelingInput.addEventListener('input', (e) => {
+    feelingValue = e.target.value;
+    feelingOutput.innerHTML = "Ich fühle mich " + feelingValue + "/10"; 
+});
+
 

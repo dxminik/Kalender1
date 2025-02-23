054b58aab98d4dd56c4f9e3592b08cc4b930778f
SamysSabber <118390382+SamysSabber@users.noreply.github.com>
2025-02-24 00:11:48 +0100

    Einträge löschen

--- backend/static/eintraege.js
+++ backend/static/eintraege.js
@@ -1,6 +1,7 @@
 const anzeige = document.getElementById('anzeigeContent');
 
-let eintraege;
+let eintrag = 0;
+const anzahlEintraege = 0;
 
 fetch('http://127.0.0.1:5000/getEvents', {
     method: 'GET',
@@ -31,11 +32,26 @@ fetch('http://127.0.0.1:5000/getEvents', {
             <p class="anzeigeDescription">Beschreibung: ${description}</p>
             <p class="anzeigeEmpfehlung">Empfehlung: ${empfehlung}</p>
             <p class="anzeigeDate">Erstellt am: ${date} ${time}</p> 
-            <button class="anzeigeDelete" value="${id}" id="button${id}"><svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e8eaed"><path d="M280-120q-33 0-56.5-23.5T200-200v-520h-40v-80h200v-40h240v40h200v80h-40v520q0 33-23.5 56.5T680-120H280Zm400-600H280v520h400v-520ZM360-280h80v-360h-80v360Zm160 0h80v-360h-80v360ZM280-720v520-520Z"/></svg></button>
+            <button class="anzeigeDelete" id="button${id}" value="${id}" onClick="deleteEintrag(${id})"><svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e8eaed"><path d="M280-120q-33 0-56.5-23.5T200-200v-520h-40v-80h200v-40h240v40h200v80h-40v520q0 33-23.5 56.5T680-120H280Zm400-600H280v520h400v-520ZM360-280h80v-360h-80v360Zm160 0h80v-360h-80v360ZM280-720v520-520Z"/></svg></button>
             `;
+            eintrag++;
             anzeige.appendChild(div);
         })
     })
-})  
+})
 
-
+function deleteEintrag(id) {
+    fetch('http://127.0.0.1:5000/deleteEvent', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            id: id
+        })
+    }).then((response) => {
+        response.json().then((data) => {
+            console.log(data)
+        })
+    })
+}
